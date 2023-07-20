512d089d182eb98457dfbd9820e86a2f3e8d0377
kittiphatnon <6330036021@student.chula.ac.th>
2023-07-20 13:28:37 +0700

    invoice added

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const quaotation = require('./quaotation');
- 
-app.get('/pdf', (req, res) => {
+const invoice = require('./invoice');
+ //Quaotation
+/* app.get('/pdf', (req, res) => {
   quaotation.generateQuatation((err, data) => {
+    if (err) {
+      res.status(500).send('Error generating PDF');
+    } else {
+      res.setHeader('Content-Type', 'application/pdf');
+      res.send(data);
+    }
+  });
+}); */
+
+//Invoice
+app.get('/pdf', (req, res) => {
+  invoice.generateInvoice((err, data) => {
     if (err) {
       res.status(500).send('Error generating PDF');
     } else {
