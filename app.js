a2922639001f17e166069e15232d631378394939
kittiphatnon <6330036021@student.chula.ac.th>
2023-07-20 11:57:19 +0700

    test upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+const pdfGenerator = require('./pdfGenerator');
+ 
+app.get('/pdf', (req, res) => {
+  pdfGenerator.generatePDF((err, data) => {
+    if (err) {
+      res.status(500).send('Error generating PDF');
+    } else {
+      res.setHeader('Content-Type', 'application/pdf');
+      res.send(data);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
